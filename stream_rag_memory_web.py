7705c2921e3bda2d3d1e1767f869c3468351b26d
msnoh1 <msnoh@pknu.ac.kr>
2025-04-07 03:52:32 +0900

    Update stream_rag_memory_web.py

--- stream_rag_memory_web.py
+++ stream_rag_memory_web.py
@@ -51,7 +51,7 @@ def get_vectorstore(_docs):
 # PDF 문서 로드-벡터 DB 저장-검색기-히스토리 모두 합친 Chain 구축
 @st.cache_resource
 def initialize_components(selected_model):
-    file_path = r"/mount/src/langchain_streamlit_chatbot/대한민국헌법(헌법)(제00010호)(19880225).pdf"
+    file_path = r"/mount/src/langchain_streamlit_chatbot/file1.pdf"
     pages = load_and_split_pdf(file_path)
     vectorstore = get_vectorstore(pages)
     retriever = vectorstore.as_retriever()
